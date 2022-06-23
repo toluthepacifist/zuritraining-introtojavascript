ec3261d4351b984a95b80951ca5c9509902404c8
Tolu Ajao <tolurere@gmail.com>
2022-06-23 06:23:35 +0100

    First commit

---
+++ name.js
@@ -0,0 +1,6 @@
+const name = "Tolulope Ajao"
+document.write("My name is " +name);
+const height = "5'11"
+document.write("<P>Height: " +height);
+const country = "Nigeria"
+document.write("<P>Country: " +country)
